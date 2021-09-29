3aaa0b1bbc73f9fb6972434aa14c05a59245dd7f
hdogrul <hdogrul@MacBook-Pro.local>
2021-09-29 08:28:56 -0400

    adding user route

---
+++ routes/api/user-routes.js
@@ -0,0 +1,27 @@
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/user-controller');
+
+// /api/users
+router
+.route('/')
+.get(getAllUsers)
+.post(createUser)
+
+// /api/users/:id
+router
+.route('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser)
+
+router
+.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend)
