e57fcd668e356aac0a5c99eeceb11c1cf622bb15
hdogrul <hdogrul@MacBook-Pro.local>
2021-09-30 12:03:44 -0400

    updating server js

--- server.js
+++ server.js
@@ -8,11 +8,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialnetwork', {
-  useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+  // useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+if (mongoose.connection.readyState === 0) {
+  console.log("0: disconnected");
+} else if (mongoose.connection.readyState === 1) {
+  console.log("1: connected");
+} else if (mongoose.connection.readyState === 2) {
+  console.log("2: connecting");
+} else {
+  console.log("3: disconnecting");
+}
+  
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
